27bb3a3bd4f2eee20c0ba73455f48a0a67da408d
Raviv Cohen <rcohen@isecpartners.com>
2014-01-29 14:09:35 -0500

    Updated homebrew install location

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -11,7 +11,7 @@
 # Homebrew should already be installed at this point.
 if [[ ! "$(type -P brew)" ]]; then
   e_header "Installing Homebrew"
-  true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+  true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
 if [[ "$(type -P brew)" ]]; then
