6c28f0bce606311a9a94836d1c6b5f868fa4f811
Raviv Cohen <rcohen@isecpartners.com>
2014-01-29 14:05:44 -0500

    Forgot that if git is already installed then main dotfiles bin wont install homebrew pushing back homebrew install into osx init file

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -8,11 +8,11 @@
 #   sudo xcode-select -switch /usr/bin
 # fi
 
-# # Homebrew should already be installed at this point.
-# if [[ ! "$(type -P brew)" ]]; then
-#   e_header "Installing Homebrew"
-#   true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-# fi
+# Homebrew should already be installed at this point.
+if [[ ! "$(type -P brew)" ]]; then
+  e_header "Installing Homebrew"
+  true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+fi
 
 if [[ "$(type -P brew)" ]]; then
   e_header "Updating Homebrew"
