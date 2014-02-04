9df79e3205d0b0ea65d021dd5ad8529aab85543a
Raviv Cohen <rcohen@isecpartners.com>
2014-02-04 11:38:15 -0500

    Fixed VirtualENvWrapper

--- link/.oh-my-zsh-custom/development.zsh
+++ link/.oh-my-zsh-custom/development.zsh
@@ -10,27 +10,23 @@ alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET
 # OSX
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     ##Python and virtual ENVS Stuff!!
-    PATH=/usr/local/share/python/:$PATH
-    export WORKON_HOME=~/.virtualenvs
-    source /usr/local/share/python/virtualenvwrapper.sh
-    export PIP_VIRTUALENV_BASE=$WORKON_HOME
-    export PIP_RESPECT_VIRTUALENV=true
-
-    ##???
-    # # Before other PATHs...
-    # PATH=${PATH}:/usr/local/share/python
-    # 
-    # # Python
-    # export WORKON_HOME=$HOME/.virtualenvs
-    # export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
-    # export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+    # PATH=/usr/local/share/python/:$PATH
+    # export WORKON_HOME=~/.virtualenvs
+    # source /usr/local/share/python/virtualenvwrapper.sh
     # export PIP_VIRTUALENV_BASE=$WORKON_HOME
     # export PIP_RESPECT_VIRTUALENV=true
-    # if [[ -r /usr/local/share/python/virtualenvwrapper.sh ]]; then
-    #     source /usr/local/share/python/virtualenvwrapper.sh
-    # else
-    #     echo "WARNING: Can't find virtualenvwrapper.sh"
-    # fi
+
+    # Python
+    export WORKON_HOME=$HOME/.virtualenvs
+    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+    export PIP_RESPECT_VIRTUALENV=true
+    if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
+        source /usr/local/bin/virtualenvwrapper.sh
+    else
+        echo "WARNING: Can't find virtualenvwrapper.sh"
+    fi
 
 
 # Ubuntu.
