f05666da7968214a5f47bb94f399b5c295eabb06
Raviv Cohen <rcohen@isecpartners.com>
2014-02-04 10:56:34 -0500

    Added path_remove function back

--- link/.oh-my-zsh-custom/osx.zsh
+++ link/.oh-my-zsh-custom/osx.zsh
@@ -1,6 +1,20 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
+# Utilities, helpers. Moved into here just to make it cleaner
+# From http://stackoverflow.com/questions/370047/#370255
+function path_remove() {
+  IFS=:
+  # convert it to an array
+  t=($PATH)
+  unset IFS
+  # perform any array operations to remove elements from the array
+  t=(${t[@]%%$1})
+  IFS=:
+  # output the new array
+  echo "${t[*]}"
+}
+
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
 PATH=/usr/local/bin:$(path_remove /usr/local/bin)
 export PATH
