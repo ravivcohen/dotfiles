d40a78e388c5340a5f7110e10c89ab91cf904961
Ben Alman <EMAIL>
2014-05-24 20:42:57 -0400

    updated sha

--- conf/osx/fonts/font-dejavu-sans-mono-for-powerline.rb
+++ conf/osx/fonts/font-dejavu-sans-mono-for-powerline.rb
@@ -5,7 +5,7 @@ class FontDejavuSansMonoForPowerline < Cask
       :trust_cert => true
   homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/DejaVuSansMono'
   version '1.010'
-  sha256 'be11b7ac056ecaac75fd5787d4f173fc9b8c85608adef4146aae388941bed9f5'
+  sha256 '81daf214fb5e5a0176681a92256d9077886ffb048ceba1231add010dfd6b9cd6'
   font 'DejaVu Sans Mono for Powerline.ttf'
   font 'DejaVu Sans Mono Bold for Powerline.ttf'
   font 'DejaVu Sans Mono Oblique for Powerline.ttf'
