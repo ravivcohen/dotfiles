64a43cecbfcc96f63e1526a6630752ed2219c4a1
ravivcohen <ravivcohen@gmail.com>
2015-02-22 22:45:37 -0500

    Typo fix 1

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -17,7 +17,7 @@ if [[ ! "$(type -P brew)" ]]; then
   true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   e_header "Installing Homebrew pks on first run"
 fi
-
+echo ~
 # Just incase we set the path again over here.
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
 PATH=/usr/local/bin:$(path_remove /usr/local/bin)
