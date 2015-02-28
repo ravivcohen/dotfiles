6964482a140ef2b4ff3e27d32eae23df66f76fcb
ravivcohen <ravivcohen@gmail.com>
2015-02-28 18:16:19 -0500

    Added spotlight stuff needs more testing

--- conf/osx/conf_osx_global.sh
+++ conf/osx/conf_osx_global.sh
@@ -30,7 +30,7 @@ sudo defaults write com.apple.spotlight orderedItems -array \
     '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
     '{"enabled" = 1;"name" = "DIRECTORIES";}' \
     '{"enabled" = 1;"name" = "PDF";}' \
-    '{"enabled" = 1;"name" = "FONTS";}' \
+    '{"enabled" = 0;"name" = "FONTS";}' \
     '{"enabled" = 1;"name" = "DOCUMENTS";}' \
     '{"enabled" = 0;"name" = "MESSAGES";}' \
     '{"enabled" = 0;"name" = "CONTACT";}' \
@@ -39,8 +39,8 @@ sudo defaults write com.apple.spotlight orderedItems -array \
     '{"enabled" = 0;"name" = "BOOKMARKS";}' \
     '{"enabled" = 0;"name" = "MUSIC";}' \
     '{"enabled" = 0;"name" = "MOVIES";}' \
-    '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-    '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+    '{"enabled" = 1;"name" = "PRESENTATIONS";}' \
+    '{"enabled" = 1;"name" = "SPREADSHEETS";}' \
     '{"enabled" = 0;"name" = "SOURCE";}' \
     '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
     '{"enabled" = 0;"name" = "MENU_OTHER";}' \
