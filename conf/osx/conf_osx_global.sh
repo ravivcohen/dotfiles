14767dc727b41b9821247768715b233f26e93a17
ravivcohen <ravivcohen@gmail.com>
2015-03-01 16:24:30 -0500

    moved functions back out

--- conf/osx/conf_osx_global.sh
+++ conf/osx/conf_osx_global.sh
@@ -1,6 +1,3 @@
-# All Helper functions can now be found inside libs/helper_functions.
-. $lib_file
-
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "0x326c333374"
 sudo scutil --set HostName "0x326c333374"
