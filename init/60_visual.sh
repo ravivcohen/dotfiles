497f25328ad364ba4bf4596b3b8d2ee869aa403b
ravivcohen <ravivcohen@gmail.com>
2015-03-02 10:22:58 -0500

    Removed osx terminal setup i use iterm2 only

--- init/60_visual.sh
+++ init/60_visual.sh
@@ -18,39 +18,6 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     defaults write  ~/Library/Preferences/com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool TRUE
     defaults write  ~/Library/Preferences/com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iTerm/";
     
-	# Download all the needed themes.
-    # Theme URL array
-    themes_array=("https://github.com/tomislav/osx-terminal.app-colors-solarized/raw/master/Solarized%20Dark.terminal"
-            "https://github.com/chriskempson/tomorrow-theme/raw/master/OS%20X%20Terminal/Tomorrow%20Night.terminal"
-            "https://github.com/chriskempson/tomorrow-theme/raw/master/OS%20X%20Terminal/Tomorrow%20Night%20Eighties.terminal"
-            #"https://github.com/chriskempson/tomorrow-theme/raw/master/iTerm2/Tomorrow%20Night.itermcolors"
-            #"https://github.com/chriskempson/tomorrow-theme/raw/master/iTerm2/Tomorrow%20Night%20Eighties.itermcolors"
-            #"https://github.com/altercation/solarized/raw/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors"
-            )
-    for i in "${themes_array[@]}"
-    do
-            :
-            # Strip https:/ and leave /
-            url_name="$(echo $i | sed 's~http[s]*:/~~g')"
-            # Pick last group in the path
-            filename=${url_name##/*/}
-            if [[ "${filename##*.}" == "itermcolors" &&
-                "$(defaults read com.googlecode.iterm2 | grep -i -c "${filename%.*}")" != "0" ]]; then
-                continue
-            elif [[ "${filename##*.}" == "terminal" &&
-                "$(defaults read com.apple.terminal | grep -i -c "${filename%.*}")" != "0" ]]; then
-                continue
-            fi
-            e_header "Installing theme: $filename"
-            # Get the File
-            curl -fsSL $i -o $filename
-            # Open file so it get init and sleep 1 to give it time
-            open $filename
-            sleep 1
-            # Delete the no longer needed file.
-            rm $filename
-
-    done
 
 # Ubuntu.
 elif [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
