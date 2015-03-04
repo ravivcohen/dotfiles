1eafd1f94ec4c41a609c2b5855f36e587694c7c3
ravivcohen <ravivcohen@gmail.com>
2015-03-04 18:03:20 -0500

    Link conf files

--- init/99_osx.sh
+++ init/99_osx.sh
@@ -3,3 +3,8 @@
 e_header "Running OSX Config"
 # OSX Config. Can safely be run everytime.
 source $DOTFILES_HOME/.dotfiles/conf/osx/conf_osx.sh
+
+if [ -d "$DOTFILES_HOME/conf/dotfiles" ]; then
+    e_header "Linking CONF files"
+    ln -sf $DOTFILES_HOME/conf/dotfiles/* $DOTFILES_HOME/
+fi
