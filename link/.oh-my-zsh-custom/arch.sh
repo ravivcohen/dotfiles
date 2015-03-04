2e1e2c5da1358fcf9179562a5207ec0322a38c7d
ravivcohen <ravivcohen@gmail.com>
2015-03-04 18:55:13 -0500

    more arch stuff

--- link/.oh-my-zsh-custom/arch.sh
+++ link/.oh-my-zsh-custom/arch.sh
@@ -1,6 +1,10 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Arch ]] || return 1
 
+if [[ $(whoami) == "vagrant" ]]; then
+    export DISPLAY=:7
+fi
+
 # Package management
 alias update="sudo pacman --noc -Syyu"
 alias install="sudo pacman -S"
