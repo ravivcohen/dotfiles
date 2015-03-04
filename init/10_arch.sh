7093b8d684d26545aaa5094ff02645bd8431e637
ravivcohen <ravivcohen@gmail.com>
2015-03-04 16:03:05 -0500

    First try at arch

---
+++ init/10_arch.sh
@@ -0,0 +1,29 @@
+if [[ "$(cat /etc/issue 2> /dev/null)" =~ Arch ]]; then
+
+    #All Helper functions can now be found inside libs/helper_functions.
+    . $lib_file
+
+    # Update APT.
+    e_header "Updating pacman"
+    pacman --noc -Syyu
+
+    # Install APT packages.
+    packages=(
+      zsh
+      htop
+      python2
+    )
+
+    list=()
+    for package in "${packages[@]}"; do
+      if [[ ! "$(pacman -Q "$package" 2>/dev/null | grep "^$package")" ]]; then
+        list=("${list[@]}" "$package")
+      fi
+    done
+
+    if (( ${#list[@]} > 0 )); then
+      e_header "Installing APT packages: ${list[*]}"
+      for package in "${list[@]}"; do
+        sudo pacman --noc -S "$package"
+      done
+    fi
