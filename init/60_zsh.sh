831cfabddfab8a8bfbb020948cad1f2d80293c95
Raviv Cohen <ravivcohen@gmail.com>
2016-02-07 22:23:39 -0800

    Update 60_zsh.sh

--- init/60_zsh.sh
+++ init/60_zsh.sh
@@ -2,9 +2,8 @@
 if [[ ! -e $USER_HOME/.oh-my-zsh ]]; then
     # 2. Second We Setup OH-MY-ZSH  
     e_header "Setup oh-my-zsh"
-    export PATH="/usr/local/bin:$PATH"
     ##SETUP OH MY ZSH ONLY ON THE FIRST TIME
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(export PATH=\"/usr/local/bin:$PATH\"; curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 if is_arch; then
