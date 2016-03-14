f2896563936a5fcad3dfb1f9423a7d776dd888ba
Raviv Cohen <ravivcohen@gmail.com>
2016-03-14 01:54:21 -0400

    Manual OH-MY-ZSH install due to some weird change in robbys install script

--- init/60_zsh.sh
+++ init/60_zsh.sh
@@ -3,7 +3,8 @@ if [[ ! -e $USER_HOME/.oh-my-zsh ]]; then
     # 2. Second We Setup OH-MY-ZSH  
     e_header "Setup oh-my-zsh"
     ##SETUP OH MY ZSH ONLY ON THE FIRST TIME
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    chsh -s /bin/zsh
 fi
 
 if is_arch; then
